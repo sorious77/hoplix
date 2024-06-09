33abd431f342114f8383d5c7d00c7a2817f488d8
sorious77 <sorious77@naver.com>
2024-06-09 18:29:49 +0900

    :bug: text overflow 수정

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -10,8 +10,12 @@ export default function Credit({ credit }: { credit: ICredit }) {
         alt={credit.name}
         className='rounded-2xl h-5/6 bg-white'
       />
-      <div className='text-sm mt-2'>{credit.character}</div>
-      <div className='text-xs text-gray-400 mb-2'>{credit.name}</div>
+      <div className='text-sm mt-2 text-ellipsis overflow-hidden whitespace-nowrap'>
+        {credit.character}
+      </div>
+      <div className='text-xs text-gray-400 mb-2 text-ellipsis overflow-hidden whitespace-nowrap'>
+        {credit.name}
+      </div>
     </div>
   )
 }
