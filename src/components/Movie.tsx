2b43393d76fb385ad4254cd3b5cbf0c27adc6c9a
sorious77 <sorious77@naver.com>
2024-06-09 17:00:11 +0900

    :lipstick: 영화 목록 스타일 수정

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export default function Movie(movie: IMovie) {
   return (
-    <div className='mb-2 md:mb-10'>
+    <div className='mb-2 md:mb-10 hover:scale-105 ease-in-out duration-300'>
       <Link href={`/movie/${movie.id}`}>
-        <h1 className='text-xs md:text-lg text-ellipsis overflow-hidden whitespace-nowrap'>
+        <h1 className='text-sm md:text-lg text-ellipsis overflow-hidden whitespace-nowrap pl-1 mb-1'>
           {movie.title}
         </h1>
         <img
           src={movie.backdrop_path}
           alt={movie.title}
-          className='w-100 rounded-lg'
+          className='w-11/12 rounded-lg'
         />
       </Link>
     </div>
