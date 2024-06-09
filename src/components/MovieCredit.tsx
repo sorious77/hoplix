6c7cfdd7e3f0017b47b1c7e2891e387c9425a24f
sorious77 <sorious77@naver.com>
2024-06-09 17:28:57 +0900

    :lipstick: credit 배역명 추가

--- src/components/MovieCredit.tsx
+++ src/components/MovieCredit.tsx
@@ -6,7 +6,8 @@ export default function MovieCredit({ credit }: { credit: ICredit }) {
         alt={credit.name}
         className='rounded-2xl h-5/6'
       />
-      <div className='text-sm'>{credit.name}</div>
+      <div className='text-sm mt-2'>{credit.character}</div>
+      <div className='text-xs text-gray-400 mb-2'>{credit.name}</div>
     </div>
   )
 }
